59a61fcbce105ca4cb519f3cb23d11d816f25c5c
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-06-05 14:04:36 +0000

    make express use index.html as root file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var html_dir = './html/';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('index.html', { root: './html' })
 });
 
 module.exports = router;
