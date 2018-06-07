c8d0ad07ce697bd9d0723d9f1858376c3417f894
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-06-07 16:14:37 +0000

    update table.js

--- routes/table.js
+++ routes/table.js
@@ -2,18 +2,22 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var app = express();
+
 var instances;
+var id;
+class MyEmitter extends EventEmitter {};
+const myEmitter = new MyEmitter();
 
 router.get('/', function(req, res, next) {
   deploy_instance();
-  res.render('table-datatable.handlebars', {instances: instances});
+  res.render('table-datatable.handlebars', {instances: instances})
 });
 
 function deploy_instance(){
   request.post('http://109.236.88.80:3003/relays/deploy', (err, res, body) => {
   if(err){ return console.log(err); };
-  console.log(res.body);
-  instances = res.body;})
+  instances = res.body;
+  id = res.body.id})
 };
 
 module.exports = router;
