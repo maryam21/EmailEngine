2d00cdf024dd544b8d1cf0b42cc0c1f83098f2dc
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-06-07 12:53:08 +0000

    fire api call to deploy new instance when accessing tables

--- routes/table.js
+++ routes/table.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var app = express();
 
 router.get('/', function(req, res, next) {
   res.render('table-datatable.handlebars');
+  deploy_instance();
 });
+
+function deploy_instance(){
+  request.post('http://109.236.88.80:3003/relays/deploy', (err, res, body) => {
+  if(err){ return console.log(err); }
+  console.log(res.body);
+})};
 
 module.exports = router;
