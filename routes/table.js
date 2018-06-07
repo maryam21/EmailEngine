ff29828385fa7389905536db873222e7f1424c70
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-06-07 14:31:14 +0000

    pass res.body to page

--- routes/table.js
+++ routes/table.js
@@ -2,16 +2,18 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var app = express();
+var instances;
 
 router.get('/', function(req, res, next) {
-  res.render('table-datatable.handlebars');
   deploy_instance();
+  res.render('table-datatable.handlebars', {instances: instances});
 });
 
 function deploy_instance(){
   request.post('http://109.236.88.80:3003/relays/deploy', (err, res, body) => {
-  if(err){ return console.log(err); }
+  if(err){ return console.log(err); };
   console.log(res.body);
-})};
+  instances = res.body;})
+};
 
 module.exports = router;
