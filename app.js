32141536bac905c7d0010e962bcd976169fe8c16
Mariam Ahhttouche <mariam2ac1@gmail.com>
2018-06-08 12:25:59 +0000

    add route for add domain action

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var tableRouter = require('./routes/table');
+var domainRouter = require('./routes/domain');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/table', tableRouter);
+app.use('/domain', domainRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
